86182cebc127667af6af9098eb375119c5b88914
Cristian Angulo <cristian.angulo@siesa.com>
2021-11-19 10:55:26 -0500

    develop branch sprint

--- app/src/main/java/com/dev/appturist/MainActivity.kt
+++ app/src/main/java/com/dev/appturist/MainActivity.kt
@@ -2,7 +2,7 @@ package com.dev.appturist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//njhb
+//njhba
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
